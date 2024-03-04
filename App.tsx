b23a6825dfa0945a53378b0032ceba5e1503cb59
Muhammet Emin Demiralay <pepmuhammet.42@gmail.com>
2024-03-04 19:19:32 +0300

    AWS integration

--- App.tsx
+++ App.tsx
@@ -1,6 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import Router from './src/router';
-export default function App() {
+
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+import {
+  withAuthenticator,
+  useAuthenticator
+} from '@aws-amplify/ui-react-native';
+
+function App() {
   return <Router />
 }
 
+
+export default withAuthenticator(App)
